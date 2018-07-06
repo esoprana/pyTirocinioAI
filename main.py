0e4c7a1ad98040eaf40b77f858e42203cfac23a7
Enrico Soprana <esoprana@gmail.com>
2018-07-06 14:33:58 +0200

    Add heroku

--- main.py
+++ main.py
@@ -1,5 +1,26 @@
+print('prova')
+
 import api
+import os
+import sys
 
 if __name__ == '__main__':
-    api.app.run(debug=True)
+    envDebug: str = os.environ.get('DEBUG')
+    debug: bool = (envDebug is not None) and (envDebug == '1')
+
+    try:
+        port = int(os.environ.get('PORT', 5000))
+    except ValueError:
+        print('Enviroment varible port should be number')
+        sys.exit(5)
+
+    if (port >= 65535):
+        print('Enviroment varible port is not valid port')
+        sys.exit(4)
+
+    print(envDebug, port)
+
+
+    port = int(os.environ.get("PORT", 5000))
+    api.app.run(host='0.0.0.0', port=port, debug=debug)
 
